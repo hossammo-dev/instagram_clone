2ad81abc8c831a1406765aa23d7c9ac80ac2d2a8
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-09-11 13:00:18 +0200

    set up activity secreen

--- lib/modules/activity/activity_screen.dart
+++ lib/modules/activity/activity_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/shared/constants.dart';
 
+import 'package:timeago/timeago.dart' as timeago;
+
+import '../../models/activity_model.dart';
+import '../../shared/cubit/main_cubit/main_cubit.dart';
 import '../../shared/widgets/cached_image.dart';
 import '../../shared/widgets/components.dart';
 
@@ -17,21 +20,32 @@ class ActivityScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(12.0),
-          child: ListView.builder(
-            itemCount: 100,
-            physics: const BouncingScrollPhysics(),
-            itemBuilder: (context, index) => Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: ListTile(
-                leading: CachedImage(
-                  imageUrl: Constants.userImage,
-                  circle: true,
-                ),
-                title: const Text(Constants.dummyText),
-                subtitle: const Text('1w'),
-              ),
-            ),
-          ),
+          child: FutureBuilder<List<ActivityModel>>(
+              future: MainCubit.get(context).getActivities(),
+              builder: (context, activities) {
+                if (activities.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else {
+                  return ListView.builder(
+                    itemCount: activities.data?.length,
+                    physics: const BouncingScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      final _activity = activities.data![index];
+                      return Padding(
+                      padding: const EdgeInsets.only(bottom: 16.0),
+                      child: ListTile(
+                        leading: CachedImage(
+                          imageUrl: _activity.avatarUrl!,
+                          circle: true,
+                        ),
+                        title: Text(_activity.activityTitle!),
+                        subtitle: Text(timeago.format(_activity.time!)),
+                      ),
+                    );
+                    },
+                  );
+                }
+              }),
         ),
       ),
     );
